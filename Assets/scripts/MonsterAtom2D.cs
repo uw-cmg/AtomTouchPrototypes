2368110b01c8a325366fcd9364fe7a39856d0b57
Pirolf <yuchengtu.uwm@gmail.com>
2015-05-07 15:25:23 -0500

    anchors done

--- Assets/scripts/MonsterAtom2D.cs
+++ Assets/scripts/MonsterAtom2D.cs
@@ -1,22 +1,41 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-//for cosmos riff
+//for cosmos riff and connect monsters
 public class MonsterAtom2D : Atom2D {
 	
 	void Awake(){
-		
+		base.Awake();
+		Atom2D[] atomComps = gameObject.GetComponents<Atom2D>();
+		if(atomComps.Length == 1){
+			if(charge == 0){
+				charge = 6;
+			}
+		}
+
+		if(Application.loadedLevelName == "ConnectMonsters"){
+			InitColorsByCharge();
+			//if is not anchorable (aka. fixed monster)
+			if(GetComponent<AnchorAtom>() == null){
+				GetComponent<SpriteRenderer>().color = normalColor;
+			}
+			pathHighlighter = transform.Find("PathHighlighter").gameObject;
+			
+		}
+	}
+	public void InitColorsByCharge(){
+		if(charge > 0){
+			normalColor = new Color(67f/255f, 70f/255f, 130f/255f, 1f);
+			highlightColor = new Color(131f/255f, 138f/255f, 255f/255f, 1f);
+		}else{
+			normalColor = new Color(195f/255f, 174f/255f, 140f/255f, 1f);
+			highlightColor = new Color(255f/255f, 228f/255f, 183f/255f, 1f);
+		}
 	}
 	// Use this for initialization
 	void Start () {
 		//base.Start();
-		if(charge == 0){
-			charge = 6;
-		}
-		if(Application.loadedLevelName == "ConnectMonsters"){
-			GetComponent<SpriteRenderer>().color = normalColor;
-			pathHighlighter = transform.Find("PathHighlighter").gameObject;
-		}
+		
 		
 	}
 	
--- Assets/scripts/devTools/AnchorAtom.cs
+++ Assets/scripts/devTools/AnchorAtom.cs
@@ -1,13 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+//[RequireComponent (typeof (Atom2D))]
+
 public class AnchorAtom : MonoBehaviour {
 	public float lastClickTime;
 	public bool clickIsForAddingAtom;
+	public Atom2D atomComp;
+	public MonsterAtom2D monsterAtomComp;
+
 	// Use this for initialization
 	void Start () {
 		clickIsForAddingAtom = true;
 		lastClickTime = -1.0f;
+		atomComp = GetComponent<Atom2D>();
+		monsterAtomComp = gameObject.AddComponent<MonsterAtom2D>() as MonsterAtom2D;
+		if(atomComp.charge > 0){
+			monsterAtomComp.charge = 6;
+		}else{
+			monsterAtomComp.charge = -6;
+		}
+		monsterAtomComp.InitColorsByCharge();
+		monsterAtomComp.enabled = false;
+		atomComp.enabled = true;
+
 	}
 	
 	// Update is called once per frame
@@ -21,23 +37,38 @@ public class AnchorAtom : MonoBehaviour {
 			lastClickTime = -1.0f;
 		}else if(lastClickTime <= 0){
 			lastClickTime = Time.time;
-		}else if(Time.time - lastClickTime < 0.3f){
-			Debug.Log("double click");
-			ToggleAnchor();
-			lastClickTime = -1.0f;
+		}else{ 
+			if(Time.time - lastClickTime < 0.3f){
+				Debug.Log("double click");
+				ToggleAnchor();
+				lastClickTime = -1.0f;
+			}else{
+				lastClickTime = Time.time;
+			}
+			
 		}
 		
 	}
 	void ToggleAnchor(){
 		if(gameObject.tag == "Atom"){
-			gameObject.tag = "AnchorAtom";
+			gameObject.tag = "MonsterAnchor";
 			//AtomPhysics2D.self.Ions.Remove(gameObject);
 			GetComponent<Rigidbody2D>().velocity = Vector2.zero;
 			GetComponent<Rigidbody2D>().isKinematic = true;
-		}else if(gameObject.tag == "AnchorAtom"){
+			
+			monsterAtomComp.enabled = true;
+			atomComp.enabled = false;
+			GetComponent<SpriteRenderer>().color = monsterAtomComp.normalColor;
+
+		}else if(gameObject.tag == "MonsterAnchor"){
 			gameObject.tag = "Atom";
 			//AtomPhysics2D.self.Ions.Add(gameObject);
 			GetComponent<Rigidbody2D>().isKinematic = false;
+			
+			monsterAtomComp.enabled = false;
+			atomComp.enabled = true;
+
+			GetComponent<SpriteRenderer>().color = atomComp.normalColor;
 		}	
 	}
 }
